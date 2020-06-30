1f15361f371196451d9b46d1f59bd9c8501100f8
Minseok <minseok.choi@kaist.ac.kr>
2020-06-30 02:59:53 -0700

    add bert model

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 import os
-
+os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
 import torch
 import torch.nn as nn
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-from transformers import BertTokenizerFast, BertForSequenceClassification, AdamW, BertConfig
+from torch.utils.data import DataLoader
+from transformers import BertForSequenceClassification, BertTokenizerFast
+
+import utils
+from dataset import MBTIDataset, MBTIBatchSampler, mbti_collate_fn
+from bertviz.bertviz import head_view
 
 from tqdm import tqdm, trange
 
-import utils
 
-
-def train(model, train_loader, val_loader, optimizer, model_name):
+def train(model, train_loader, val_loader, optimizer):
 
     train_losses, train_accuracies = [], []
     val_losses, val_accuracies = [], []
@@ -20,17 +22,22 @@ def train(model, train_loader, val_loader, optimizer, model_name):
     for epoch in trange(epochs, desc="Epoch"):
         train_loss, train_acc = [], []
         model.train()
-        for posts, masks, labels in tqdm(train_loader, desc="Training Iteration"):
+        for posts, attention_mask, labels in tqdm(train_loader, desc="Training Iteration"):
+            posts = posts.to(device)
+            attention_mask = attention_mask.to(device)
+            labels = labels.to(device)
+            
             optimizer.zero_grad()
-            posts, masks, labels = posts.to(device), masks.to(device), labels.to(device)
-            loss, logits = model(posts, attention_mask=masks, labels=labels)
+            loss, logits = model(posts, attention_mask=attention_mask, labels=labels)
+            loss = loss.mean()
             loss.backward()
             optimizer.step()
+            # scheduler.step()
 
             preds = torch.argmax(logits.data, dim=1)
             acc = (preds == labels).sum().item() / len(labels)
 
-            train_loss.append(loss.sum().item())
+            train_loss.append(loss.item())
             train_acc.append(acc)
 
             des1 = 'Training Loss: {:06.4f}'.format(loss.cpu())
@@ -38,19 +45,28 @@ def train(model, train_loader, val_loader, optimizer, model_name):
             loss_log.set_description_str(des1)
             acc_log.set_description_str(des2)
             del loss
-
-        train_losses.append(sum(train_loss) / len(train_loss))
-        train_accuracies.append(sum(train_acc) / len(train_acc))
+        
+        avg_train_loss = sum(train_loss) / len(train_loss)
+        avg_train_acc = sum(train_acc) / len(train_acc)
+        print()
+        print("Average Training Loss:", avg_train_loss)
+        print("Average Training Acc:", avg_train_acc)
+        print()
+        train_losses.append(avg_train_loss)
+        train_accuracies.append(avg_train_acc)
     
         val_loss, val_acc = [], []
         model.eval()
         with torch.no_grad():
-            for posts, masks, labels in tqdm(val_loader, desc="Validation Iteration"):
-                posts, masks, labels = posts.to(device), masks.to(device), labels.to(device)
-                loss, logits = model(posts, attention_mask=masks, labels=labels)
-                
-                preds = torch.argmax(logits.data, dim=1)
-                acc = (preds == labels).sum().item() / len(labels)
+            for posts, attention_mask, labels in tqdm(val_loader, desc="Validation Iteration"):
+                posts = posts.to(device)
+                attention_mask = attention_mask.to(device)
+                labels = labels.to(device)
+
+                loss, logits = model(posts, attention_mask=attention_mask, labels=labels)
+                loss = loss.mean()
+                pred = torch.argmax(logits.data, dim=1)
+                acc = (pred == labels).sum().item() / len(labels)
                 
                 val_loss.append(loss.item())
                 val_acc.append(acc)
@@ -60,12 +76,16 @@ def train(model, train_loader, val_loader, optimizer, model_name):
                 loss_log.set_description_str(des1)
                 acc_log.set_description_str(des2)
                 del loss
+        
+        avg_val_loss = sum(val_loss) / len(val_loss)
+        avg_val_acc = sum(val_acc) / len(val_acc)
+        print()
+        print("Average Validation Loss:", avg_val_loss)
+        print("Average Validation Acc:", avg_val_acc)
+        print()
+        val_losses.append(avg_val_loss)
+        val_accuracies.append(avg_val_acc)
 
-        val_losses.append(sum(val_loss) / len(val_loss))
-        val_accuracies.append(sum(val_acc) / len(val_acc))
-
-        torch.save(model.state_dict(), model_name + '_epoch' + str(epoch) + '.pth')
-    
     return train_losses, val_losses, train_accuracies, val_accuracies
 
 
@@ -83,45 +103,51 @@ def predict(model, test_loader):
 
 if __name__ == "__main__":
     # Tune hyperparameters here
-    batch_size = 32         # 16 or 32          (recommended in BERT paper)
+    batch_size = 16         # 16 or 32          (recommended in BERT paper)
     epochs = 4              # 2, 3, or 4        
-    learning_rate = 2e-5    # 5e-5, 3e-5, 2e-5
-    device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
+    learning_rate = 5e-5    # 5e-5, 3e-5, 2e-5
+    torch.manual_seed(0)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Running on:", device)
 
-    # Load pretrained BERT model & tokenizer
-    tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased', do_lower_case=True)
-    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=16, output_attentions=False, output_hidden_states=False)
+    # Load pretrained BERT model
+    model_name = "16classes_per_person_bert_aug"
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=16)
+    tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+    model = nn.DataParallel(model)
     model.to(device)
-    model_name = "16classes_per_post"
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
-
-    # # Load & store data into memory
-    # posts_train, masks_train, labels_train = utils.load_tsv('./preprocess_new/split_train/mbti.tsv', tokenizer)
-    # posts_val, masks_val, labels_val = utils.load_tsv('./preprocess_new/split_val/mbti.tsv', tokenizer)
     
-    # train_set = TensorDataset(posts_train, masks_train, labels_train)
-    # val_set = TensorDataset(posts_val, masks_val, labels_val)
+    # Load & store data into memory    
+    train_set = MBTIDataset('./preprocess_minseok/per_person_for_bert/mbti_train.tsv', tokenizer)
+    val_set = MBTIDataset('./preprocess_minseok/per_person_for_bert/mbti_val.tsv', tokenizer)
+    # train_set = MBTIDataset('./gina/personal_data_aug_train.tsv', tokenizer)
+    # val_set = MBTIDataset('./gina/personal_data_aug_valid.tsv', tokenizer)
 
-    # train_loader = DataLoader(train_set, sampler=RandomSampler(train_set), batch_size=batch_size)
-    # val_loader = DataLoader(val_set, sampler=SequentialSampler(val_set), batch_size=batch_size)
+    train_batch_sampler = MBTIBatchSampler(train_set, batch_size=batch_size, shuffle=True)
+    val_batch_sampler = MBTIBatchSampler(val_set, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_set, batch_sampler=train_batch_sampler, collate_fn=mbti_collate_fn)
+    val_loader = DataLoader(val_set, batch_sampler=val_batch_sampler, collate_fn=mbti_collate_fn)
 
-    # # Train & save the model
-    # train_losses, val_losses, train_accuracies, val_accuracies = train(model, train_loader, val_loader, optimizer, model_name)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=len(train_loader), num_training_steps=len(train_loader)*epochs)
 
-    # # Plot training & validation losses and accuracies
-    # utils.plot_values(train_losses, val_losses, title=model_name+"_losses")
-    # utils.plot_values(train_accuracies, val_accuracies, title=model_name+"_accuracies")
+    # Train & save the model
+    train_losses, val_losses, train_accuracies, val_accuracies = train(model, train_loader, val_loader, optimizer)
+    torch.save(model.state_dict(), model_name+'.pth')
+
+    # Plot training & validation losses and accuracies
+    utils.plot_values(train_losses, val_losses, title=model_name+"_losses")
+    utils.plot_values(train_accuracies, val_accuracies, title=model_name+"_accuracies")
     
-    # print("Final training loss: {:06.4f}".format(train_losses[-1]))
-    # print("Final validation loss: {:06.4f}".format(val_losses[-1]))
-    # print("Final training accuracy: {:06.4f}".format(train_accuracies[-1]))
-    # print("Final validation accuracy: {:06.4f}".format(val_accuracies[-1]))
+    print("Final training loss: {:06.4f}".format(train_losses[-1]))
+    print("Final validation loss: {:06.4f}".format(val_losses[-1]))
+    print("Final training accuracy: {:06.4f}".format(train_accuracies[-1]))
+    print("Final validation accuracy: {:06.4f}".format(val_accuracies[-1]))
 
     # Evaluate on test data
-    posts_test, masks_test, labels_test = utils.load_tsv('./preprocess_new/split_test/mbti.tsv', tokenizer)
-    test_set = TensorDataset(posts_test, masks_test, labels_test)
-    test_loader = DataLoader(test_set, sampler=SequentialSampler(test_set), batch_size=batch_size)
-    model.load_state_dict(torch.load(model_name+"_epoch3.pth"), strict=False)
-    test_accuracy = predict(model, test_loader)
-    print("Test accuracy: {:06.4f}".format(test_accuracy))
+    # posts_test, masks_test, labels_test = utils.load_tsv('./preprocess_new/split_test/mbti.tsv', tokenizer)
+    # test_set = TensorDataset(posts_test, masks_test, labels_test)
+    # test_loader = DataLoader(test_set, sampler=SequentialSampler(test_set), batch_size=batch_size)
+    # model.load_state_dict(torch.load(model_name+"_epoch3.pth"), strict=False)
+    # test_accuracy = predict(model, test_loader)
+    # print("Test accuracy: {:06.4f}".format(test_accuracy))
--- utils.py
+++ utils.py
@@ -5,39 +5,6 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def encode_label(label):
-    label_dict = {
-        "ISTJ": 0, "ISFJ": 1, "INFJ": 2, "INTJ": 3,
-        "ISTP": 4, "ISFP": 5, "INFP": 6, "INTP": 7,
-        "ESTP": 8, "ESFP": 9, "ENFP": 10, "ENTP": 11,
-        "ESTJ": 12, "ESFJ": 13, "ENFJ": 14, "ENTJ": 15
-    }
-    return label_dict[label]
-
-
-def load_tsv(tsv_file, tokenizer):
-    posts, masks, labels = [], [], []
-    max_len = 0
-    with open(tsv_file, "r") as f:
-        reader = csv.reader(f, delimiter="\t")
-        for label, post in tqdm(reader, desc="Reading "+tsv_file):
-            encoded_post = tokenizer.encode_plus(
-                                post,
-                                add_special_tokens=True,    # add [CLS] and [SEP] tokens 
-                                max_length=128,             # pad & truncate to T=128
-                                pad_to_max_length=True,
-                                return_attention_mask=True, # construct attention masks
-                                return_tensors='pt',        # return pytorch tensors
-                           )
-            posts.append(encoded_post['input_ids'])
-            masks.append(encoded_post['attention_mask'])
-            labels.append(encode_label(label))
-    posts = torch.cat(posts, dim=0)
-    masks = torch.cat(masks, dim=0)
-    labels = torch.tensor(labels)
-    return posts, masks, labels
-
-
 def plot_values(train_values, val_values, title):
     x = list(range(1, len(train_values)+1))
     plt.figure()
